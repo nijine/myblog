fb4e64fe30bbffc650e486a75048392675761a5f
Dmitriy Loshakov <winterdrift@gmail.com>
2023-01-13 06:59:58 +0000

    Add infra folder

---
+++ infra/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {
+    bucket = "nijine-terraform"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.49"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "site" {
+  source      = "github.com/nijine/simple-cf-site"
+  domain_name = "loshakov.link"
+  www_dir     = "../www"
+}
