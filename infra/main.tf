fac568c4588dc4ff6d901df6b97ac7dda11b83f9
Dmitriy Loshakov <winterdrift@gmail.com>
2023-01-24 18:02:31 -0500

    Add a rewrite function to properly redirect to pages without .html in the link

--- infra/main.tf
+++ infra/main.tf
@@ -23,4 +23,24 @@ module "site" {
   source       = "github.com/nijine/simple-cf-site"
   domain_name  = "loshakov.link"
   error_object = "404.html"
+
+  default_cache_func_assoc = [{
+    event_type   = "viewer-request"
+    function_arn = aws_cloudfront_function.rewrite_uri.arn
+  }]
+}
+
+resource "aws_cloudfront_function" "rewrite_uri" {
+  name    = "rewrite-request-if-page-without-dot-html"
+  runtime = "cloudfront-js-1.0"
+  code    = <<EOF
+function handler(event) {
+  var request = event.request;
+  var uri = request.uri;
+
+  request.uri = `$${uri.includes(".") ? uri : uri.concat(".html")}`;
+
+  return request;
+}
+EOF
 }
